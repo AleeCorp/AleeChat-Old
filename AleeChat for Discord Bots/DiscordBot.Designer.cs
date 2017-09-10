d8944c600d168ac3da4c0acebf4eae5491038d20
Unknown <Alee14498@gmail.com>
2017-09-10 15:11:42 -0400

    Added project files

---
+++ AleeChat for Discord Bots/DiscordBot.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace AleeChat_for_Discord_Bots
+{
+    partial class DiscordBot
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DiscordBot));
+            this.label1 = new System.Windows.Forms.Label();
+            this.discordConsole = new System.Windows.Forms.RichTextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.sendButton = new System.Windows.Forms.Button();
+            this.guildComboBox = new System.Windows.Forms.ComboBox();
+            this.channelBox = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tokenBox = new System.Windows.Forms.TextBox();
+            this.loginButton = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.settingsButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(44, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(188, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Welcome to AleeChat for Discord Bots";
+            // 
+            // discordConsole
+            // 
+            this.discordConsole.Location = new System.Drawing.Point(33, 50);
+            this.discordConsole.Name = "discordConsole";
+            this.discordConsole.ReadOnly = true;
+            this.discordConsole.Size = new System.Drawing.Size(665, 289);
+            this.discordConsole.TabIndex = 1;
+            this.discordConsole.Text = resources.GetString("discordConsole.Text");
+            this.discordConsole.TextChanged += new System.EventHandler(this.discordConsole_TextChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(79, 347);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(498, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // sendButton
+            // 
+            this.sendButton.Location = new System.Drawing.Point(596, 344);
+            this.sendButton.Name = "sendButton";
+            this.sendButton.Size = new System.Drawing.Size(75, 23);
+            this.sendButton.TabIndex = 3;
+            this.sendButton.Text = "Send";
+            this.sendButton.UseVisualStyleBackColor = true;
+            this.sendButton.Click += new System.EventHandler(this.sendButton_Click);
+            // 
+            // guildComboBox
+            // 
+            this.guildComboBox.FormattingEnabled = true;
+            this.guildComboBox.Location = new System.Drawing.Point(177, 424);
+            this.guildComboBox.Name = "guildComboBox";
+            this.guildComboBox.Size = new System.Drawing.Size(121, 21);
+            this.guildComboBox.TabIndex = 5;
+            this.guildComboBox.SelectedIndexChanged += new System.EventHandler(this.guildComboBox_SelectedIndexChanged);
+            // 
+            // channelBox
+            // 
+            this.channelBox.FormattingEnabled = true;
+            this.channelBox.Location = new System.Drawing.Point(374, 424);
+            this.channelBox.Name = "channelBox";
+            this.channelBox.Size = new System.Drawing.Size(121, 21);
+            this.channelBox.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(135, 427);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Guilds";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(317, 427);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Channels";
+            // 
+            // tokenBox
+            // 
+            this.tokenBox.Location = new System.Drawing.Point(79, 377);
+            this.tokenBox.Name = "tokenBox";
+            this.tokenBox.Size = new System.Drawing.Size(498, 20);
+            this.tokenBox.TabIndex = 9;
+            this.tokenBox.TextChanged += new System.EventHandler(this.tokenBox_TextChanged);
+            // 
+            // loginButton
+            // 
+            this.loginButton.Location = new System.Drawing.Point(596, 374);
+            this.loginButton.Name = "loginButton";
+            this.loginButton.Size = new System.Drawing.Size(75, 23);
+            this.loginButton.TabIndex = 10;
+            this.loginButton.Text = "Login";
+            this.loginButton.UseVisualStyleBackColor = true;
+            this.loginButton.Click += new System.EventHandler(this.loginButton_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(44, 350);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Chat";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(35, 380);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(38, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Token";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(339, 21);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(61, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Alpha 0.0.1";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(603, 21);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(95, 13);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Made By AleeCorp";
+            // 
+            // settingsButton
+            // 
+            this.settingsButton.Location = new System.Drawing.Point(531, 422);
+            this.settingsButton.Name = "settingsButton";
+            this.settingsButton.Size = new System.Drawing.Size(75, 23);
+            this.settingsButton.TabIndex = 15;
+            this.settingsButton.Text = "Settings";
+            this.settingsButton.UseVisualStyleBackColor = true;
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(623, 422);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 23);
+            this.exitButton.TabIndex = 16;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // DiscordBot
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(734, 457);
+            this.ControlBox = false;
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.settingsButton);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.loginButton);
+            this.Controls.Add(this.tokenBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.channelBox);
+            this.Controls.Add(this.guildComboBox);
+            this.Controls.Add(this.sendButton);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.discordConsole);
+            this.Controls.Add(this.label1);
+            this.Name = "DiscordBot";
+            this.Text = "Launcher";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox discordConsole;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button sendButton;
+        private System.Windows.Forms.ComboBox guildComboBox;
+        private System.Windows.Forms.ComboBox channelBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tokenBox;
+        private System.Windows.Forms.Button loginButton;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button settingsButton;
+        private System.Windows.Forms.Button exitButton;
+    }
+}
+
---
+++ AleeChat for Discord Bots/DiscordBot.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Discord;
+using Discord.WebSocket;
+
+namespace AleeChat_for_Discord_Bots
+{
+    public partial class DiscordBot : Form
+    {
+        DiscordSocketClient Client;
+
+        public DiscordBot()
+        {
+            InitializeComponent();
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void guildComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            channelBox.Items.Clear();
+
+            foreach(var Guild in Client.Guilds)
+            {
+                if(Guild.Name == guildComboBox.Text)
+                {
+                    foreach(var Tc in Guild.TextChannels)
+                    {
+                        channelBox.Items.Add(Tc.Name);
+                    }
+                }
+            }
+        }
+
+        private async void loginButton_Click(object sender, EventArgs e)
+        {
+            Client = new DiscordSocketClient(new DiscordSocketConfig()
+            {
+                LogLevel = LogSeverity.Verbose
+            });
+
+            Client.Log += Client_Log;
+
+            try
+            {
+                await Client.LoginAsync(TokenType.Bot, tokenBox.Text);
+                await Client.StartAsync();
+            }
+            catch
+            {
+                MessageBox.Show("Error occured while connecting your Bot!", "ERROR");
+                return;
+            }
+            await Task.Delay(3000);
+
+            foreach (var Guild in Client.Guilds)
+            {
+                guildComboBox.Items.Add(Guild.Name);
+            }
+
+            guildComboBox.SelectedIndexChanged += guildComboBox_SelectedIndexChanged;
+        }
+
+        private Task Client_Log(LogMessage arg)
+        {
+            Invoke((Action)delegate
+            {
+                discordConsole.AppendText(arg + "\n");
+            });
+            return null;
+        }
+
+        private void discordConsole_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tokenBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void sendButton_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
